88215c369d7caef12dc2afd8df8d6c07363261ba
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-04-15 16:52:35 -0400

    done no stretch

---
+++ src/Nasa.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Nasa() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=Uqb7Xj3AUngMG9kUuUIM5iNlQ1wcvsXPEfuzrA6b`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [data]);
+  return (
+    <div>
+      <div>
+        <img src={data.hdurl}></img>
+      </div>
+      <h2> {data.title}</h2>
+      <h3>{data.date} </h3>
+      <p>{data.explanation} </p>
+    </div>
+  );
+}
+
+export default Nasa;
